4e3252a948e9b2a4de658974e464abd231b308b8
Raheem Kawojue <kawojue08@gmail.com>
2023-03-17 00:56:15 +0100

    [add] header component

---
+++ app/Header.tsx
@@ -0,0 +1,11 @@
+const Header: React.FC = () => {
+    return (
+        <header className="w-full bg-blue-500 px-10 py-5">
+            <nav className="p-2 bg-blue-300 rounded-md mx-auto">
+                Navbar
+            </nav>
+        </header>
+    )
+}
+
+export default Header
