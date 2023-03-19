acf96ddde33a8463f240f29d69a685701ed032fa
Raheem Kawojue <kawojue08@gmail.com>
2023-03-19 14:08:34 +0100

    .

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
     return (
         <header className="w-full bg-blue-500 px-10 py-5">
             <nav className="flex justify-between items-center px-5 tracking-wider">
-                <p>TODOS</p>
+                <Link href="/todos">TODOS</Link>
                 <Link href="/" title="home" className="rounded-lg text-ls bg-white text-blue-500 py-0.5 px-2 font-medium hover:text-black trans-0">
                     Home
                 </Link>
