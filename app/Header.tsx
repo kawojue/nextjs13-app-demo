7107ecaf5eadbe042cf7e4e04c7f6534373eafa8
Raheem Kawojue <kawojue08@gmail.com>
2023-03-20 00:07:10 +0100

    .

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,8 +3,11 @@ import Link from "next/link"
 const Header: React.FC = () => {
     return (
         <header className="w-full bg-blue-500 px-10 py-5">
-            <nav className="flex justify-between items-center px-5 tracking-wider">
-                <Link href="/todos">TODOS</Link>
+            <nav className="flex justify-between items-center px-5">
+                <div className="flex gap-5 tracking-wider">
+                    <Link href="/todos">TODOS</Link>
+                    <Link href="/search">Search</Link>
+                </div>
                 <Link href="/" title="home" className="rounded-lg text-ls bg-white text-blue-500 py-0.5 px-2 font-medium hover:text-black trans-0">
                     Home
                 </Link>
