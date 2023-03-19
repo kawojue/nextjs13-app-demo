d6623e7734a925c3d5c6248a23247c8296056dba
Raheem Kawojue <kawojue08@gmail.com>
2023-03-20 00:12:15 +0100

    [add] search page

--- app/search/Search.tsx
+++ app/search/Search.tsx
@@ -1,11 +1,11 @@
 "use client"
-import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context'
 import { useRouter } from 'next/navigation'
 import React, { useState, FormEvent, useEffect, useRef } from 'react'
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context'
 
 const Search: React.FC = () => {
     const router: AppRouterInstance = useRouter()
-    const [search, setSearch] = useState("")
+    const [search, setSearch] = useState<string>("")
     const searchRef = useRef<HTMLInputElement>(null)
 
     useEffect(() => {
@@ -17,14 +17,15 @@ const Search: React.FC = () => {
         router.push(`/search/${search}`)
         setSearch("")
     }
+
     return (
         <form onSubmit={handleSearch} className="flex gap-5">
             <input value={search} type="text" ref={searchRef}
             onChange={e => setSearch(e.target.value)}
-            placeholder='Enter the search term..'
+            placeholder='Enter your search term..'
             className="p-0.5 border-none"/>
             <button type="submit"
-            className='rounded-lg text-ls bg-blue-500 text-white py-0.5 px-2 font-medium hover:text-black trans-0 tracking-wider'> Search</button>
+            className='rounded-lg text-ls bg-blue-500 text-white py-0.5 px-2 font-medium hover:text-black trans-0 tracking-wider'>Search</button>
         </form>
     )
 }
