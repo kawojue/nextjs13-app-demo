2ea055e4f47d86ff525ddfd37ad1cfe8cff565c6
Raheem Kawojue <kawojue08@gmail.com>
2023-03-21 11:26:36 +0100

    .

---
+++ app/(users)/todos/[todoId]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="w-full h-screen grid place-items-center">
+            <h1>404</h1>
+            <p>Not found.</p>
+        </div>
+    )
+}
